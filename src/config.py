dd98956db833b8f621befedb407263bf279b161b
LunaBitar1998 <bitarluna5@gmail.com>
2025-02-02 17:35:00 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -8,12 +8,12 @@ VAL_DIR = "/kaggle/input/skinlesionbinary/val/val"
 MODEL_NAME = "efficientnet_b0"          # Change to "efficientnet_b0", etc., as needed
 IMG_SIZE = 224                   # Image size for resizing
 BATCH_SIZE =32                # Batch size
-NUM_EPOCHS = 30                  # Number of training epochs
+NUM_EPOCHS = 10                  # Number of training epochs
 LEARNING_RATE = 1e-4             # Learning rate
 WEIGHT_DECAY = 1e-4              # Weight decay for regularization
 OPTIMIZER_NAME = "adam"          # Optimizer: "adam", "sgd"
 LOSS_NAME = "bce"                # Loss function: "bce", "cross_entropy"
-PATIENCE = 5                     # Patience for early stopping
+PATIENCE = 3                     # Patience for early stopping
 DROPOUT= 0.5
 # Device
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
